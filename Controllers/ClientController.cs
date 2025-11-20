b96f8b21c3e9da0f2ccb184b90d83a2c83f43738
Francisco Galea <galeafrancisco2001@gmail.com>
2025-11-20 14:29:46 -0300

    implemented controllers

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using BankSimulator.Models;
+
+namespace BankSimulator.Controllers
+{
+    internal class ClientController
+    {
+        public void CreateClient()
+        {
+            Client client = new();
+            Console.WriteLine("Ingrese su dni: ");
+            client.dni = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nombre: ");
+            client.name = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido: ");
+            client.lastName = Console.ReadLine();
+            Console.WriteLine("Ingrese su email: ");
+            client.email = Console.ReadLine();
+            Console.WriteLine("Ingrese su contraseña: ");
+        }
+    }
+}
---
+++ Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BankSimulator.Controllers
+{
+    internal class MainController
+    {
+        public void Menu()
+        {
+            Console.WriteLine("Bienvenidos al sistema GARCArio!");
+        }
+    }
+}
