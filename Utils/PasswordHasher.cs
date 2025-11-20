8db8a29d3733e2d8cd01d4d3e344bd47d76a2388
Francisco Galea <galeafrancisco2001@gmail.com>
2025-11-20 14:30:31 -0300

    Implemented BCrypt for hash passwords

---
+++ Utils/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BankSimulator.Utils
+{
+    internal class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            return hashedPassword;
+        }
+
+        public bool ValidPassword(string password, string hashedPassword)
+        {
+            bool isValid = BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+            return isValid;
+        }
+
+    }
+}
