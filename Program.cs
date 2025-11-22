233f2cc06c67676ef28f68209f80e89cecf769f6
Francisco Galea <galeafrancisco2001@gmail.com>
2025-11-22 16:59:07 -0300

    Minor changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿Console.WriteLine("Hello world!");
+﻿using BankSimulator.Controllers;
+using BankSimulator.Models;
 
+List<Client> clients = new List<Client>();
+
+Console.WriteLine("Hello world!");
+ClientController controller = new ClientController(clients);
+controller.CreateClient();
