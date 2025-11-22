1321eb0911c2e4e7583e511a22cf084ea99253ef
Francisco Galea <galeafrancisco2001@gmail.com>
2025-11-22 16:59:22 -0300

    Implementing hash and verify password

--- Utils/PasswordHasher.cs
+++ Utils/PasswordHasher.cs
@@ -1,15 +1,15 @@
 ﻿
 namespace BankSimulator.Utils
 {
-    internal class PasswordHasher
+    internal static class PasswordHasher
     {
-        public string HashPassword(string password)
+        public static string HashPassword(string password)
         {
             string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
             return hashedPassword;
         }
 
-        public bool ValidPassword(string password, string hashedPassword)
+        public static bool ValidPassword(string password, string hashedPassword)
         {
             bool isValid = BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
             return isValid;
