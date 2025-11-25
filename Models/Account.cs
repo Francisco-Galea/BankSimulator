1d35e676544361219cd34ea8dfe5fa4c197c5817
Francisco <galeafrancisco2001@gmail.com>
2025-11-25 12:59:12 -0300

    Minimal change

--- Models/Account.cs
+++ Models/Account.cs
@@ -5,9 +5,8 @@ namespace BankSimulator.Models
     {
         public int accountId {  get; set; }
         public int clientId { get; set; }
-        //public Client client { get; set; }
         public decimal balance { get; set; }
-        public AccountType accountType { get; set; }
+        public List<AccountType> accountType { get; set; }
         public DateOnly accountCreation { get; set; }
     }
 
