e939ca8282870912b388ed1d6a4c834d4baf12f5
Francisco <galeafrancisco2001@gmail.com>
2025-11-25 12:58:59 -0300

    Implementing UnitOfWork pattern

---
+++ UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using BankSimulator.Models;
+using BankSimulator.Repositories;
+
+namespace BankSimulator.UnitOfWorks
+{
+    internal class UnitOfWork
+    {
+        private readonly IRepository<Account> accountRepository = new AccountRepository();
+        private readonly IRepository<Client> clientRepository = new ClientRepository();
+
+        public void SaveAccountAndClient(Account account, Client client)
+        {
+            accountRepository.SaveData(account);
+            clientRepository.SaveData(client);
+        }
+
+
+    }
+}
