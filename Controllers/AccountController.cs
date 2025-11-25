444ab6ea9157402cf3ebba81092c927b6eefb2b7
Francisco <galeafrancisco2001@gmail.com>
2025-11-25 13:00:09 -0300

    Implemmenting create Account

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using BankSimulator.Models;
+using BankSimulator.UnitOfWorks;
+
+namespace BankSimulator.Controllers
+{
+    internal class AccountController
+    {
+
+        private readonly UnitOfWork unitOfWork = new();
+
+
+        public void CreateAccount(Client client)
+        {
+            Account account = new Account();
+            account.accountId = client.clientId;
+            account.clientId = client.clientId;
+            account.accountCreation = DateOnly.FromDateTime(DateTime.Now);
+            account.balance = 0;
+            Console.WriteLine("Que tipo de cuenta desea crear? " +
+                "\n1- Corriente " +
+                "\n2- Ahorro " +
+                "\n3- Ambos " +
+                "\n4- Cancelar operacion");
+            int accountOption = int.Parse(Console.ReadLine());
+            account.accountType = SelectAccountType(accountOption);
+            unitOfWork.SaveAccountAndClient(account, client);
+        }
+
+        public List<AccountType> SelectAccountType(int accountOption)
+        {
+            List<AccountType> accountTypes = new List<AccountType>();
+            switch (accountOption)
+            {
+                case 1:
+                    accountTypes.Add(AccountType.Corriente);
+                    break;
+                case 2:
+                    accountTypes.Add(AccountType.Ahorro);
+                    break;
+                case 3:
+                    accountTypes.Add(AccountType.Ahorro);
+                    accountTypes.Add(AccountType.Corriente);
+                    break;
+                case 4:
+                    Console.WriteLine("Cancelando operacion, espere");
+                    break;
+                default:
+                    Console.WriteLine("Opcion ingresada invalida, intente nuevamente");
+                    break;
+            }
+            return accountTypes;
+        }
+
+
+    }
+}
