817353fc449159573ac73a98ae1937d55818bca9
Francisco <galeafrancisco2001@gmail.com>
2025-11-27 16:24:21 -0300

    Learning to handle exceptions

---
+++ ExceptionHandler/ClientExceptions/ClientNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BankSimulator.ExceptionHandler.ClientExceptions
+{
+    internal class ClientNotFoundException : Exception
+    {
+        public ClientNotFoundException()
+        {
+            Console.WriteLine("Cliente no encontrado");
+        }
+    }
+}
