81d7c76535cb38bfdd4ce8c6a007925660586ccf
Francisco <galeafrancisco2001@gmail.com>
2025-12-01 16:30:14 -0300

    Implementing get/set C# properties, to improve Encapsulation

--- Models/Account.cs
+++ Models/Account.cs
@@ -3,11 +3,49 @@ namespace BankSimulator.Models
 {
     internal sealed class Account
     {
-        public int accountId {  get; set; }
-        public int clientId { get; set; }
-        public decimal balance { get; set; }
-        public List<AccountType> accountType { get; set; }
-        public DateOnly accountCreation { get; set; }
+        private int accountId;
+        private int clientId;
+        private decimal balance;
+        private List<AccountType> accountType;
+        private DateOnly accountCreation;
+
+        public int ACCOUNTID
+        {
+            get => this.accountId;
+            set => this.accountId = value;
+        }
+
+        public int CLIENTID
+        {
+            get => this.clientId;
+            set => this.clientId = value;
+        }
+
+        public decimal BALANCE
+        {
+            get => this.balance;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("El balance no puede ser menos de $1.-");
+                }
+                this.balance = value;
+            }
+        }
+
+        public List<AccountType> ACCOUNTTYPE
+        {
+            get => this.accountType;
+            set => this.accountType = value;
+        }
+
+        public DateOnly ACCOUNTCREATION
+        {
+            get => this.accountCreation;
+            set => this.accountCreation = value;
+        }
+
     }
 
     internal enum AccountType
@@ -15,5 +53,7 @@ namespace BankSimulator.Models
         Ahorro,
         Corriente
     }
+
+ 
 
 }
