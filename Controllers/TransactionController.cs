e98ca21cd9b117d2a0a9eeb52044961dc54ee6a0
Francisco <galeafrancisco2001@gmail.com>
2025-12-03 14:27:41 -0300

    Implementing Withdraw, Deposit and Transfer methods

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,10 +1,31 @@
-﻿
+﻿using BankSimulator.Models;
+using BankSimulator.Repositories;
+using BankSimulator.Repositories.Interfaces;
+
 namespace BankSimulator.Controllers
 {
     internal sealed class TransactionController
     {
 
-        public void WithDraw()
+        private readonly ITransactionRepository transactionRepository = new TransactionRepository();
+
+        public void WithDraw(Client clientLoged)
+        {
+            Transaction transaction = new();
+            transaction.originAccountId = clientLoged.clientId;
+            Console.WriteLine($"Saldo disponible: \nIngrese la cantidad a retirar:");
+            transaction.ammount = decimal.Parse(Console.ReadLine());
+            transaction.transactionType = TransactionType.Retiro;
+            transaction.dateTimeOfTransaction = DateTime.UtcNow;
+            transactionRepository.SaveData(transaction);
+        }
+
+        public void Deposit(Client clientLoged)
+        {
+
+        }
+
+        public void Transfer(Client clientLoged)
         {
 
         }
